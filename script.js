4bd898145201cda57c02595a57c402ef9786d74d
unknown <pal05249@gmail.com>
2020-04-13 17:39:20 +0530

    additon of files

---
+++ script.js
@@ -0,0 +1,10 @@
+/* {let person1 = {
+    this.mass=prompt("Enter mark's mass ");
+    this.height=prompt("Enter mark's height ");
+    this.fullname="MarkSmith";
+}
+console.log(person1); */
+
+
+
+"terminal.integrated.shell.windows": "D:\\Git\\bin\\bash.exe"
